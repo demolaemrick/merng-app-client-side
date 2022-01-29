912a4c5a9e1255fec87b6b9419a2d378f3346014
Emrick19 <emricky17@gmail.com>
2022-01-29 21:42:55 +0100

    change server base url

--- src/index.js
+++ src/index.js
@@ -4,29 +4,27 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { setContext } from 'apollo-link-context'
+import { setContext } from "apollo-link-context";
 
 import App from "./App";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:8080",
+  uri: "https://gentle-escarpment-73904.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
-  const token = localStorage.getItem('Token');
-  return{
+  const token = localStorage.getItem("Token");
+  return {
     headers: {
-      Authorization: token ? `Bearer ${token}` : ''
-    }
-  }
-})
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  
 });
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
